43cd4b20d350727746489b63f29ff94e58e437a0
Olga Martinikhina <martinikhina@gmail.com>
2021-04-05 21:35:45 +0300

    files added

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+// TODO: write code here
+// eslint-disable-next-line import/extensions
+import Collapse from './collapse.js';
+
+const collapse = new Collapse();
+collapse.init();
---
+++ src/js/collapse.js
@@ -0,0 +1,11 @@
+export default class Collapse {
+    // eslint-disable-next-line class-methods-use-this
+    init() {
+      const btnCollapse = document.querySelector('.collapseButton');
+      const elCOllapse = document.querySelector('.collapse');
+  
+      btnCollapse.addEventListener('click', () => {
+        elCOllapse.classList.toggle('active');
+      });
+    }
+  }
